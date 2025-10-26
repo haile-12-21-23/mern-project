c90f9b4d263a17de2bc187b120587466274d7267
Hailemariam addisu <105885701+haile-12-21-23@users.noreply.github.com>
2025-10-26 13:07:51 +0300

    Initiating server file

--- backend/server.js
+++ backend/server.js
@@ -9,4 +9,10 @@ const app=express();
 app.use(express.json());
 app.use(cors());
 
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(()=>console.log(`MongoDB connected successfully.`))
+.catch(error=>console.log(`Error occurred:${error}`));
+const port=process.env.PORT||3000;
+app.listen(port, ()=>console.log(`Server is running at port:${port}`));
 
